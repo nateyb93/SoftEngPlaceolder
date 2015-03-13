379c1e897276023bea51b4b1fe1e8ce645c414b5
nateyb93 <brownn15@up.edu>
2015-03-13 01:16:25 -0700

    Completed functionality for test session 1

--- main.js
+++ main.js
@@ -41,28 +41,33 @@ function conditionsclick() {
     request.send();
 }
 
+//Handles the forecast click function call
 function forecastclick() {
+    //API key
     var key = "898fac3520e03d7d";
-    var hitemp = document.getElementById('hitemp');
-    var lotemp = document.getElementById('lotemp');
-    var pic = document.getElementById('pic');
-    var info = document.getElementById('forecastdetail');
-    var location = document.getElementById('location');
 
     var btnForecast = document.getElementById('forecastbutton');
     var btnCurrent = document.getElementById('conditionsbutton');
 
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://api.wunderground.com/api/e37a167f3d3d327a/forecast/q/CA/San_Francisco.json', true);
+    request.open('GET', 'http://api.wunderground.com/api/e37a167f3d3d327a/forecast10day/q/OR/Portland.json', true);
 
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
-            pic.src = data["forecast"]["simpleforecast"]["forecastday"][1]["icon_url"];
-            hitemp.innerHTML = Math.floor(data["forecast"]["simpleforecast"]["forecastday"][1]["high"]["fahrenheit"]);
-            lotemp.innerHTML = Math.floor(data["forecast"]["simpleforecast"]["forecastday"][1]["low"]["fahrenheit"]).toString();
-            info.innerHTML = data["forecast"]["simpleforecast"]["simpleforecast"]["forecastday"][1]["conditions"];
-            location.innerHTML = data["location"]["city"];
+            
+            //Removes all children from forecast div
+            var forecastDiv = document.getElementById('forecastDiv');
+            while (forecastDiv.hasChildNodes()) {
+                forecastDiv.removeChild(forecastDiv.lastChild);
+            }
+            for (var i = 0; i < 7; i++) {
+                populateForecastDay(data["forecast"]["simpleforecast"]["forecastday"][i]["date"]["weekday_short"],
+                                    Math.floor(data["forecast"]["simpleforecast"]["forecastday"][i]["high"]["fahrenheit"]),
+                                    Math.floor(data["forecast"]["simpleforecast"]["forecastday"][i]["low"]["fahrenheit"]),
+                                    data["forecast"]["simpleforecast"]["forecastday"][i]["conditions"],
+                                    data["forecast"]["simpleforecast"]["forecastday"][i]["icon_url"]);
+            }
         } else {
 
         }
@@ -73,4 +78,59 @@ function forecastclick() {
     }
 
     request.send();
+}
+
+function populateForecastDay(day, hiTempText, loTempText, conditionsText, imgSrc) {
+    //get reference to content div
+    var forecastDiv = document.getElementById('forecastDiv');
+
+    var daySpan = document.createElement('span');
+    daySpan.className = 'daySpan';
+
+    //initializes weather controls
+    var outerDiv = document.createElement('div');
+    outerDiv.className = 'outerDiv';
+
+    var weatherImage = document.createElement('div');
+    weatherImage.className = 'weatherImage';
+
+    var pic = document.createElement('img');
+    pic.className = 'pic';
+
+    var weatherInfo = document.createElement('div');
+    weatherInfo.className = 'weatherInfo';
+
+    var forecastDetail = document.createElement('span');
+    forecastDetail.className = 'forecastDetail';
+
+    var temps = document.createElement('div');
+    temps.className = 'temps';
+
+    var hiTemp = document.createElement('span');
+    hiTemp.className = 'hiTemp';
+
+    var loTemp = document.createElement('span');
+    loTemp.className = 'loTemp';
+
+    //set data
+    daySpan.innerHTML = day;
+    hiTemp.innerHTML = hiTempText;
+    loTemp.innerHTML = loTempText;
+    forecastDetail.innerHTML = conditionsText;
+    pic.src = imgSrc;
+
+    //build top-level structures
+    temps.appendChild(hiTemp);
+    temps.appendChild(loTemp);
+
+    weatherInfo.appendChild(forecastDetail);
+    weatherInfo.appendChild(temps);
+
+    weatherImage.appendChild(pic);
+
+    outerDiv.appendChild(daySpan);
+    outerDiv.appendChild(weatherImage);
+    outerDiv.appendChild(weatherInfo);
+
+    forecastDiv.appendChild(outerDiv);
 }
